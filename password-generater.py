593c5e1a5153e0eabdbd2927a3e950c2c0e2efd7
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-11-19 21:32:12 +0530

    updated code

--- password-generater.py
+++ password-generater.py
@@ -1,10 +1,12 @@
 import random
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
-symbols = "[]{}()*;/,._-"
+import string
 
-all = lower + upper + numbers + symbols
-length = 16
-password = "".join(random,random.sample(all,length))
-print(password)
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter the desired password length: "))
+    generated_password = generate_password(password_length)
+    print("Generated Password:", generated_password)
